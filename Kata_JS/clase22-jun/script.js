645f2363c36d3f1d383aaf8e1c1037b9571de4b6
VALENTINAMOR11 <valentina.moreno-m@outlook.com>
2023-06-23 22:55:44 -0500

    Update with new projects

---
+++ Kata_JS/clase22-jun/script.js
@@ -0,0 +1,12 @@
+console.log( 5 == 3); // -> false
+
+console.log (5 == 5); // -> true
+
+console.log ("Hola" == " Hola");  // -> false por el espacio 
+
+console.log("Hola  Adrian".length > "JAvier".length);
+
+//Operadores logicos && y ||
+
+console.log(3 > 6 && 7 > 3); // -> false
+console.log(3>6 || 7>3); // -> true  
